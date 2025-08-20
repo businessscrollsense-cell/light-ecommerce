104363c0670e5b0f31770e042fbb0fc33e59b7b4
businessscrollsense-cell <business.scrollsense@gmail.com>
2025-08-20 16:41:42 +0200

    Update svelte.config.ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,19 +1,12 @@
-import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+// svelte.config.js
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
+};
 
-	kit: {
-		adapter: adapter(),
-		csrf: {
-			checkOrigin: false
-		}
-	},
-	vitePlugin: {
-		inspector: true
-	}
-}
-
-export default config
+export default config;
